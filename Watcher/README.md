1f416acb6bacf7e0239467c99884ba2026dca1aa
VladLan13 <vladislav_lanskih@smartexlab.com>
2019-07-11 21:04:30 +0300

    Add subject of a problem

---
+++ Watcher/README.md
@@ -0,0 +1,5 @@
+**Watcher**
+
+`How Vue reactivity system works under the hood.` 
+
+This example explains how Vue understand when it should rerender values sides and Square (see index.vue) in it's template.
---
+++ Watcher/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Square Calculation</h1>
+        <div>Side <b>a</b>: {{ a }}</div>
+        <div>Side <b>b</b>: {{ b }}</div>
+        <div><b>Square:</b>: {{ a * b }}</div>
+    </div>
+</template>
+<script>
+	export default {
+		data: () => ({
+			a: 5,
+			b: 2
+		})
+
+	}
+</script>
